e46ed0df86a7433632a65d9d897c353132fd6873
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2023-10-28 13:39:41 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,58 @@
+    });
+
+        console.log("Random Number: " + item);
+
+
+function main() {
+}
+    return data;
+    for (let i = 0; i < 10; i++) {
+}
+    const data = generateRandomData();
+function generateRandomData() {
+main();
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+    }    }
+function generateRandomData() {
+
+
+main();
+    return data;
+    });
+
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+        console.log("Random Number: " + item);
+    const data = generateRandomData();
+    const data = [];
+    data.forEach(item => {
+}
+    for (let i = 0; i < 10; i++) {    }
+
+
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+        }
+}def main():
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    for item in data:
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    return data
+import random
+    main()
+
+if __name__ == "__main__":
